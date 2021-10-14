7bdbfec5e633b72446974fd34766a39ce1890840
Manali <manalipc10@gmail.com>
2021-10-14 13:26:53 +0530

    Navbar

---
+++ client/src/App.js
@@ -0,0 +1,20 @@
+import React from "react";
+import './App.css';
+import  {Header} from './components';
+import { BrowserRouter as Router, Route } from 'react-router-dom';
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+function App() {
+  return (
+    <div className="App">
+    <Router>
+      <Route exact path="/">
+        <Header/>
+      </Route>
+    </Router>
+    </div>
+  );
+}
+
+export default App;
+
---
+++ client/src/components/Header.jsx
@@ -0,0 +1,31 @@
+import React from 'react';
+import {Navbar, Nav} from 'react-bootstrap';
+import { LinkContainer } from "react-router-bootstrap";
+import "./Header.css"
+
+function Header() {
+    return (
+        <div>
+             <>
+                <Navbar className="navbar">
+                    <LinkContainer to="/">
+                        <Navbar.Brand className="name">Class Doon</Navbar.Brand>
+                    </LinkContainer>
+                    <Navbar.Toggle aria-controls="basic-navbar-nav" />
+                    <Navbar.Collapse id="basic-navbar-nav">
+                        <Nav className="components">
+                            <LinkContainer to="/About">
+                                <Nav.Link className="link">About Us</Nav.Link>
+                            </LinkContainer>
+                            <LinkContainer to="/Contact">
+                                <Nav.Link className="link">Contact Us</Nav.Link>
+                            </LinkContainer>
+                        </Nav>
+                    </Navbar.Collapse>
+                </Navbar>
+            </>
+        </div>
+    )
+}
+
+export default Header
