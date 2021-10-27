d949082c494e49958006322f4edd1cf9449e950b
Manali <manalipc10@gmail.com>
2021-10-27 21:24:35 +0530

    job added

--- client/src/components/index.js
+++ client/src/components/index.js
@@ -1,5 +1,6 @@
 export { default as Header} from './Header';
 export { default as Cards} from './Cards';
+export { default as Job} from './Job';
 export { default as About} from './About';
 export { default as Contact} from './Contact';
 export { default as Footer} from './Footer';
